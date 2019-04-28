11684178879f537a2b4e0f1418909e2df2a98102
Wolfattackx <wolf@DESKTOP-IICOQN9.localdomain>
2019-04-28 08:17:19 +0530

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var angle = 0;
+
+function setup()
+{
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+  strokeWeight(2.2);
+  reset();
+}
+
+function reset()
+{
+  var lenght = 160;
+  resetMatrix();
+  translate(width / 2, height);
+  background(120);
+  angle = random(45, 49.5);
+  //console.log(angle);
+  branch(lenght);
+}
+
+function mouseClicked()
+{
+  reset();
+}
+
+function branch(_lenght)
+{
+  var temp = random(0.5, 0.9);
+  console.log(temp);
+  line(0, 0, 0, -_lenght);
+
+  translate(0, -_lenght);
+  if(_lenght >= 4)
+  {
+    push();
+    rotate(angle * random(1));
+    stroke(255);
+    branch(_lenght * temp);
+    pop();
+    push();
+    stroke(255, 0, 0);
+    rotate(-angle * random(1));
+    branch(_lenght * temp);
+    pop();
+  }
+}
